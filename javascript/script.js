2735a6718eb53fd0e326e3cb97996b8849c184f2
Aman Deep Sharma <105082480+amansharmaa28@users.noreply.github.com>
2023-04-10 11:19:33 +0530

    added script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,6 @@
-
+var typed = new Typed(".typing",{
+    strings:[" ","Frontend Developer","Backend Developer","Web Developer","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
