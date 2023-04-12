5c3df95867a5e6249dff8b3c8f6befdb314353eb
Aman Deep Sharma <105082480+amansharmaa28@users.noreply.github.com>
2023-04-12 16:26:50 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" ","Frontend Developer","Backend Developer","Web Developer","Full Stack Developer"],
+    strings:[" ","Frontend Developer","Backend Developer","Web Developer","Pyhton Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
