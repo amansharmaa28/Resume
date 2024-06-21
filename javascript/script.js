3ed4b686059fbd904969b1ce6139f7ddf88ac95e
Aman Deep Sharma <105082480+amansharmaa28@users.noreply.github.com>
2024-06-21 17:13:26 +0530

    Update script.js
    change some text

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" ","Frontend Developer","Backend Developer","Web Developer","Pyhton Developer"],
+    strings:[" ",".Net Devloper MVC Core Developer","Frontend Developer""Pyhton Developer","Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
