8a13873e5676d49a1a37dd37923f51c3f9339445
Aman Deep Sharma <105082480+amansharmaa28@users.noreply.github.com>
2024-06-24 10:37:50 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" ",".Net Devloper MVC Core Developer","Frontend Developer""Pyhton Developer","Web Developer"],
+    strings:[" ",".Net Devloper MVC Core Developer","Frontend Developer","Pyhton Developer","Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
